d5af8d070d08d3a45fc847ca9754005744f836e7
Matias Basto <bastom1@udayton.edu>
2026-08-30 17:54:35 -0400

    created demo mini  projects

---
+++ week2/ArrayDemo.java
@@ -0,0 +1,52 @@
+package week2;
+
+public class ArrayDemo {
+
+    public static void main(String[] args) {
+        //Initialize names and score to size three, with names having "Mark", "Nick", and "John".
+        String[] names = {"Mark", "Nick", "John"};
+        int[] score = new int[3];
+        
+        //TODO: intialize everyone's score to 0
+
+        //Round 1 - Mark earns 1 point
+        System.out.println("=-=-=-=-=Round 1=-=-=-=-=");
+        //TODO: increment Mark's score by 1  
+
+        printScore(names, score);
+
+        //Round 2 - Mark and Nick earn 2 points
+        System.out.println("=-=-=-=-=Round 2=-=-=-=-=");
+        //TODO: increment Mark and Nick's score by 2
+
+        printScore(names, score);
+        
+        //Round 3 - John earns a point
+        System.out.println("=-=-=-=-=Round 3=-=-=-=-=");
+        //TODO: increment John's score by 1
+
+        printScore(names, score);
+
+        //Round 4 - John earns 3 points
+        System.out.println("=-=-=-=-=Round 4=-=-=-=-=");
+        //TODO: increment John's score by 3
+
+        printScore(names, score);
+        
+        //Round 5 - Everyone earns 1 point
+        System.out.println("=-=-=-=-=Round 5=-=-=-=-=");
+        //TODO: increment everyone's score using a for loop
+
+        printScore(names, score);
+
+        //Final Score
+        System.out.println("=-=-=-=Final Score=-=-=-=");
+        printScore(names, score);
+    }
+
+    public static void printScore(String[] names, int[] score) {
+        //TODO: use a for loop to print out the name and score of the player in format (name: score)
+
+        System.out.println();
+    }
+}
---
+++ week2/ArrayListDemo.java
@@ -0,0 +1,36 @@
+package week2;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+    public static void main(String[] args) {
+        //TODO: intialize both the item and quantity lists to size 3.
+        ArrayList<String> item;  
+        ArrayList<Integer> quantity;
+
+        //TODO: add soda to the list, with quantity 2
+
+        //TODO: add paper plates set to the list, with quantity 1
+
+        //TODO: add solo cups to the list, with quantity 3
+
+        printList(item, quantity);
+
+        //TODO: You remembered you needed paper towels, add paper towel rolls, with quantity 2
+        printList(item, quantity);
+
+        //TODO: You went to the store and got everything but cups, remove all items except for cups
+        //Hint: You need to edit both the item and quantity
+
+        printList(item, quantity);
+    }
+
+    public static void printList(ArrayList<String> items, ArrayList<Integer> quantity) {
+        System.out.println("=-=-=-=Store List=-=-=-=");
+        //TODO: Use a form loop to access items from list to print out in format: (item: quantity)
+        
+        
+        System.out.println();
+    }
+}
