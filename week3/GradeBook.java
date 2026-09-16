8e7aacad8d5a99293a528b8962c2195114484809
Matias Basto <bastom1@udayton.edu>
2026-09-16 18:30:35 -0400

    Edited practice file to remove answers

--- week3/GradeBook.java
+++ week3/GradeBook.java
@@ -8,25 +8,14 @@ public class GradeBook {
         int rejected = 0;
 
         //TODO: for each grade, try/catch/finally
-        for (int g : grades) {
-            try {
-                recordGrade(g);
-            } catch (InvalidGradeException e) {
-                System.out.println("Rejected: " + e.getMessage());
-                rejected++;
-            } finally {
-                checked++;
-            }
-        }
+        
 
         System.out.println("Checked " + checked + " grades, rejected " + rejected + " grades.");
     }
 
     static void recordGrade(int grade) throws InvalidGradeException {
         //TODO: throw InvalidGradeException if grade is invalid
-        if (grade < 0 || grade > 100) {
-            throw new InvalidGradeException("Invalid grade: " + grade);
-        }
+        
         // Code to record the valid grade
         System.out.println("Recorded grade: " + grade);
     }
